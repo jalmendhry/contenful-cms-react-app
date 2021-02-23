c04a7b798265e079848befc7ffff37a2d90bd30f
Joe Al-mendhry <joe.al-mendhry@cef.co.uky>
2021-02-23 08:18:27 +0000

    content roow styles

--- src/pages/Page.js
+++ src/pages/Page.js
@@ -6,8 +6,9 @@ import { Helmet } from 'react-helmet';
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 
 import Layout from '../components/Layout';
-
 import { client } from '../client';
+
+import './pages.css';
 
 const Page = () => {
   const { slug } = useParams();
@@ -20,6 +21,7 @@ const Page = () => {
         .getEntries({ content_type: 'page', 'fields.slug[in]': slug })
         .then(({ items }) => {
           const { fields } = items[0];
+          console.log(fields);
           setPageData(fields);
         })
         .catch(() => {
@@ -37,7 +39,7 @@ const Page = () => {
     return <Redirect to="/404" />;
   }
 
-  const { pageTitle, mainContent, carousel } = pageData;
+  const { pageTitle, mainContent, carousel, contentRow } = pageData;
   return (
     <>
       <Helmet>
@@ -61,6 +63,20 @@ const Page = () => {
         <div className="content-wrapper">
           {documentToReactComponents(mainContent)}
         </div>
+
+        {contentRow &&
+          contentRow.map(({ fields }, index) => {
+            const { content, image } = fields;
+
+            return (
+              <div className="content-row" key={index}>
+                <img src={`https://${image.fields.file.url}`} alt="" />
+                <div className="content-wrapper">
+                  {documentToReactComponents(content)}
+                </div>
+              </div>
+            );
+          })}
       </Layout>
     </>
   );
